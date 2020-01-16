5f0dc3062f2856d3f8c321d5d17e68c5be0f5f1a
Victor Fung <victor13021997@gmail.com>
2020-01-17 01:59:03 +0800

    create script for player control and adjust tile width

---
+++ MusicRhythmGame/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    
+    public GameObject Warrior; 
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetButtonDown("First Lane"))
+       {
+           Debug.Log("First Lane");
+       }
+       if (Input.GetButtonDown("Second Lane"))
+       {
+           Debug.Log("Second Lane");
+       }
+       if (Input.GetButtonDown("Third Lane"))
+       {
+           Debug.Log("Third Lane");
+       }
+       if (Input.GetButtonDown("Forth Lane"))
+       {
+           Debug.Log("Forth Lane");
+       }
+    }
+}
