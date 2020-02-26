8487fd3b45bf23fbbcf29b8588aa6fb5a0a10fac
wai kin ho <wkhoae@connect.ust.hk>
2020-02-26 12:22:08 +0800

    fix camera problem

--- MusicRhythmGame/Assets/Scripts/MinionManager.cs
+++ MusicRhythmGame/Assets/Scripts/MinionManager.cs
@@ -43,7 +43,7 @@ public class MinionManager : MonoBehaviour
             }
         }
         if (currentEscapedMinion != escapedMinion) {
-            // healthBar.GetComponent<HealthBar>().OnTakeDamage(10);
+            healthBar.GetComponent<HealthBar>().OnTakeDamage(10);
             escapedMinion = currentEscapedMinion;
         }
     }
