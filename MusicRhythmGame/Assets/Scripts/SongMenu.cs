82e7f4466afdf6383a3c0c6a2fb2094e8fa95c24
wai kin ho <wkhoae@connect.ust.hk>
2020-02-29 15:41:40 +0800

    Add onset mode

--- MusicRhythmGame/Assets/Scripts/SongMenu.cs
+++ MusicRhythmGame/Assets/Scripts/SongMenu.cs
@@ -7,6 +7,10 @@ using UnityEngine.UI;
 public class SongMenu : MonoBehaviour
 {
     public GameObject loadingScreen;
+    public GameObject songMenuScrollView;
+    public GameObject chooseModeContainer;
+    public GameObject beatModeBtn;
+    public GameObject onsetModeBtn;
     public Slider slider;
 
     // Start is called before the first frame update
@@ -15,8 +19,16 @@ public class SongMenu : MonoBehaviour
 
     }
 
-    public void ToGame(string song) {
+    public void SetSong(string song) {
         PlayerPrefs.SetString("selectedSong", song);
+        songMenuScrollView.SetActive(false);
+        chooseModeContainer.SetActive(true);
+        beatModeBtn.SetActive(true);
+        onsetModeBtn.SetActive(true);
+    }
+
+    public void ToGame(string mode) {
+        PlayerPrefs.SetString("mode", mode);
         SceneManager.LoadScene("game");
         // StartCoroutine(LoadAsynchronously());
     }
--- MusicRhythmGame/Assets/Scripts/SpectralFluxAnalyzer.cs
+++ MusicRhythmGame/Assets/Scripts/SpectralFluxAnalyzer.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class SpectralFluxInfo {
 	public float time;
 	public float spectralFlux;
